e8ea3b62be4a963e64b7b4b6c49387d536cfbf7c
PhilZProg <philzprog@gmail.com>
2022-12-14 18:37:29 +0300

    Character components set up, implementation of movement system, game mode created

---
+++ FireFightGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FireFightGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BJD51_API AFireFightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
